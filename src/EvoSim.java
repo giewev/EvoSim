6fcf1f376d628a99fa5edf5cc34445cd7d4179ae
giewev <xatu285@gmail.com>
2015-12-04 20:39:14 -0500

    Added brush selector to menu

--- src/EvoSim.java
+++ src/EvoSim.java
@@ -1,13 +1,14 @@
 import javafx.animation.Timeline;
 
+import javafx.scene.shape.Rectangle;
 import javafx.animation.*;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -15,6 +16,8 @@ public class EvoSim extends Application {
 	final static int tileSize = 10;
 	final static int mapWidth = 60;
 	final static int mapHeight = 40;
+	
+	static String currentBrush = "Dirt";
 	
 	Map tiles = new Map(mapWidth, mapHeight, tileSize);
 	
@@ -31,17 +34,10 @@ public class EvoSim extends Application {
 		EventHandler<MouseEvent> mouseHandler = e -> {
 			int xIndex = (int)(e.getX() / tileSize);
 			int yIndex = (int)(e.getY() / tileSize);
+			
 			for(int x = xIndex - 1; x <= xIndex + 1; x++){
 				for(int y = yIndex - 1; y <= yIndex + 1; y++){
-					if(e.getButton() == MouseButton.PRIMARY){
-						tiles.setTile(new Dirt(x * tileSize, y * tileSize, tileSize, tileSize));
-					}
-					else if(e.getButton() == MouseButton.SECONDARY){
-						tiles.setTile(new Stone(x * tileSize, y * tileSize, tileSize, tileSize));
-					}
-					else{
-						tiles.setTile(new Water(x * tileSize, y * tileSize, tileSize, tileSize));
-					}
+					tiles.setTile(Terrain.construct(currentBrush, x * tileSize, y * tileSize, tileSize, tileSize));
 				}
 			}
 		};
@@ -49,8 +45,29 @@ public class EvoSim extends Application {
 		scene.setOnMouseClicked(mouseHandler);
 		scene.setOnMouseDragged(mouseHandler);
 		
+		Rectangle dirtButton = new Rectangle(mapWidth * tileSize + 5, 15, 50, 50);
+		dirtButton.setFill(Color.SADDLEBROWN);
+		dirtButton.setOnMousePressed(e -> {
+			currentBrush = "Dirt";
+		});
+		
+		Rectangle stoneButton = new Rectangle(mapWidth * tileSize + 60, 15, 50, 50);
+		stoneButton.setFill(Color.GREY);
+		stoneButton.setOnMousePressed(e -> {
+			currentBrush = "Stone";
+		});
+		
+		Rectangle waterButton = new Rectangle(mapWidth * tileSize + 115, 15, 50, 50);
+		waterButton.setFill(Color.BLUE);
+		waterButton.setOnMousePressed(e -> {
+			currentBrush = "Water";
+		});
+		
 		EventHandler<ActionEvent> tick = e -> {
 			root.getChildren().clear();
+			root.getChildren().add(dirtButton);
+			root.getChildren().add(stoneButton);
+			root.getChildren().add(waterButton);
 			tiles.draw(root);
 		};
 		
--- src/Terrain.java
+++ src/Terrain.java
@@ -7,4 +7,12 @@ public abstract class Terrain {
 	public int height;
 	
 	public abstract void draw(Group screen);
+	
+	public static Terrain construct(String type, int x, int y, int newWidth, int newHeight){
+		if(type.equals("Dirt")) return new Dirt(x, y, newWidth, newHeight);
+		if(type.equals("Stone")) return new Stone(x, y, newWidth, newHeight);
+		if(type.equals("Water")) return new Water(x, y, newWidth, newHeight);
+		
+		return null;
+	}
 }
